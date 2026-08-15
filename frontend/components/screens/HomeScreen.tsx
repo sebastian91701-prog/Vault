824c113f962418ac1e8c6e7583249e688ebc6a4b
sebastian91701-prog <sebastian91701@gmail.com>
2026-08-15 12:00:23 -0500

    Fix import paths in HomeScreen component

--- frontend/components/screens/HomeScreen.tsx
+++ frontend/components/screens/HomeScreen.tsx
@@ -2,11 +2,11 @@
 
 import React from "react";
 import { Lock, User } from "lucide-react";
-import { useVault } from "../context/VaultContext";
+import { useVault } from "../../context/VaultContext";
 import { GOLD, GOLD_SOFT, SURFACE, BORDER, TEXT_MUTED, TYPE_META } from "../../lib/constants";
-import SearchBar from "./SearchBar";
-import ItemRow from "./ItemRow";
-import { EmptyState } from "./EmptyState";
+import SearchBar from "../SearchBar";
+import ItemRow from "../ItemRow";
+import { EmptyState } from "../EmptyState";
 
 const STAT_ORDER = ["document", "note", "secret", "image", "video", "link"];
 const STAT_LABELS: Record<string, string> = {
