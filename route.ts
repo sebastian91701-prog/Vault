dde71e92c865e95eb0e59e87727d274318516ed8
sebastian91701-prog <sebastian91701@gmail.com>
2026-08-15 01:32:32 -0500

    Update import path for getSupabaseServer

--- route.ts
+++ route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSupabaseServer } from "../../../lib/supabase";
+import { getSupabaseServer } from "../lib/supabase";
 
 export async function GET() {
   const supabase = getSupabaseServer();
