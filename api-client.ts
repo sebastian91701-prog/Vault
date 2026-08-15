6ee3c42e5009dea79da1416a1beec3e4544848d5
sebastian91701-prog <sebastian91701@gmail.com>
2026-08-15 00:51:57 -0500

    Update import path for types in api-client.ts

--- api-client.ts
+++ api-client.ts
@@ -1,4 +1,4 @@
-import type { VaultItem, ActivityEntry } from "./types";
+import type { VaultItem, ActivityEntry } from "../lib/types";
 
 async function json(res: Response) {
   const data = await res.json();
