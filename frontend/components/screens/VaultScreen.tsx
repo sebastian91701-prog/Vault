487d12253f9c921ee35c5f580fae321da690eea8
sebastian91701-prog <sebastian91701@gmail.com>
2026-08-15 12:49:39 -0500

    Fix import path for useVault in VaultScreen

--- frontend/components/screens/VaultScreen.tsx
+++ frontend/components/screens/VaultScreen.tsx
@@ -2,7 +2,7 @@
 
 import React, { useMemo } from "react";
 import { SlidersHorizontal, ChevronDown } from "lucide-react";
-import { useVault } from "../context/VaultContext";
+import { useVault } from "../../context/VaultContext";
 import { GOLD, SURFACE, BORDER, TEXT_MUTED, TEXT_FAINT, FILTERS } from "../../lib/constants";
 import TopBar from "../TopBar";
 import SearchBar from "../SearchBar";
