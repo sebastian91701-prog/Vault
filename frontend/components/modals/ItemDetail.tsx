80323a0f5366815afd0b393c6add37cf2397c3f4
sebastian91701-prog <sebastian91701@gmail.com>
2026-08-15 11:56:26 -0500

    Fix import paths in ItemDetail component

--- frontend/components/modals/ItemDetail.tsx
+++ frontend/components/modals/ItemDetail.tsx
@@ -2,10 +2,10 @@
 
 import React, { useState } from "react";
 import { X, Eye, EyeOff, Link as LinkIcon, Star, Trash2 } from "lucide-react";
-import type { VaultItem } from "../lib/types";
+import type { VaultItem } from "../../lib/types";
 import { GOLD, SURFACE, SURFACE_2, BORDER, TEXT_MUTED, TEXT_FAINT, TYPE_META } from "../../lib/constants";
-import { apiRevealSecret } from "../lib/api-client";
-import IconBadge from "./IconBadge";
+import { apiRevealSecret } from "../../lib/api-client";
+import IconBadge from "../IconBadge";
 
 export default function ItemDetail({
   item, onClose, onToggleFav, onDelete,
